69b80db5a0407c2c57f8c5749c6cf257eba3268d
Hugo <hugocarbiener@gmail.com>
2022-11-19 18:36:08 +0100

    Add fow variations with depth

--- unijam2022/Assets/Scripts/Utils.cs
+++ unijam2022/Assets/Scripts/Utils.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public static class Utils
 {
-    public static List<Vector2Int> directions = new List<Vector2Int>() { Vector2Int.up, Vector2Int.down, Vector2Int.left, Vector2Int.right };
+    public static List<Vector2Int> directNeihbors = new List<Vector2Int>() { Vector2Int.up, Vector2Int.down, Vector2Int.left, Vector2Int.right };
+    public static List<Vector2Int> neihbors = new List<Vector2Int>() { Vector2Int.up, Vector2Int.down, Vector2Int.left, Vector2Int.right, Vector2Int.one, -Vector2Int.one, Vector2Int.left + Vector2Int.up, Vector2Int.right + Vector2Int.down };
+    public static List<Vector2Int> radius2Sphere = new List<Vector2Int>() { Vector2Int.up, Vector2Int.down, Vector2Int.left, Vector2Int.right, Vector2Int.one, -Vector2Int.one, Vector2Int.left + Vector2Int.up, Vector2Int.right + Vector2Int.down, 2 * Vector2Int.up, 2 * Vector2Int.down, 2 * Vector2Int.left, 2 * Vector2Int.right };
+    public static List<Vector2Int> squaredRadius2Sphere = new List<Vector2Int>() { Vector2Int.up, Vector2Int.down, Vector2Int.left, Vector2Int.right, Vector2Int.one, -Vector2Int.one, Vector2Int.left + Vector2Int.up, Vector2Int.right + Vector2Int.down, 2 * Vector2Int.up, 2 * Vector2Int.down, 2 * Vector2Int.left, 2 * Vector2Int.right, 2 * Vector2Int.up + Vector2Int.left,2 * Vector2Int.up + Vector2Int.right, 2 * Vector2Int.down + Vector2Int.left, 2 * Vector2Int.down + Vector2Int.right, 2 * Vector2Int.left + Vector2Int.down, 2 * Vector2Int.left + Vector2Int.up, 2 * Vector2Int.right + Vector2Int.up, 2 * Vector2Int.right + Vector2Int.down };
     public static float RandomGaussian(float minValue = 0.0f, float maxValue = 1.0f)
     {
         float u, v, S;
