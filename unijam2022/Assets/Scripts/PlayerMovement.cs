5a5487b70cd5a069502575c468f6b658ebdda142
Eloïse Bonnet <eloise.bonnet@telecom-sudparis.eu>
2022-11-19 10:39:21 +0100

    Rock destroyed by ray implemented

--- unijam2022/Assets/Scripts/PlayerMovement.cs
+++ unijam2022/Assets/Scripts/PlayerMovement.cs
@@ -18,16 +18,28 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.AddForce(Physics.gravity * (gravityScale - 1) * rb.mass);
+        rb.AddForce((gravityScale - 1) * rb.mass * Physics.gravity);
     }
 
     private void Update()
     {
+        // Horizontal movement
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
-        transform.Translate(movement * speed * Time.deltaTime);
+        transform.Translate(speed * Time.deltaTime * movement);
+
+        // Jump
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Z))
         {
             rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+        }
+
+        // Laser
+        if (Input.GetKey(KeyCode.E))
+        {
+            if (Physics.Raycast(transform.position, Vector3.right, out RaycastHit hit))
+            {
+                hit.transform.SendMessage("HitByRay");
+            }
         }
     }
 }
