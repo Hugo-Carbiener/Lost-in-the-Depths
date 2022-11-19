1925ddb4ca77295c03586c3f35f2433d9ffa3fce
BiscuitPrime <henri.nomico@gmail.com>
2022-11-19 10:49:39 +0100

    Added basic oxygen pylone connection

---
+++ unijam2022/Assets/Scripts/Oxygen/OxygenNetwork.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ *  Component used by the Oxygen network to allow for optimisation
+ */
+public class OxygenNetwork : MonoBehaviour
+{
+
+}
--- unijam2022/Assets/Scripts/Oxygen/OxygenPyloneController.cs
+++ unijam2022/Assets/Scripts/Oxygen/OxygenPyloneController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -9,13 +10,22 @@ using UnityEngine.Rendering;
  */
 public class OxygenPyloneController : MonoBehaviour
 {
+    [Header("Player data")]
     [SerializeField] private GameObject player;
-    private OxygenModuleController oxygenController;
+    private OxygenModuleController oxygenController; //ref to player's oxygen module
 
+    [Header("Distance data")]
     [SerializeField] private float maxDistance;
     private float curDistance;
     private bool isConnectedPlayer;
 
+    [Header("Network integration")]
+    [SerializeField] private GameObject prevPylone; //reference towards previous pylone in network
+    [SerializeField] private GameObject nextPylone; //reference towards next pylone in network
+    [SerializeField] private GameObject networkConnection;
+
+    [Header("Line Renderers")]
+    private LineRenderer networkLineRenderer;
     private LineRenderer lineRenderer;
 
     private void Start()
@@ -28,19 +38,25 @@ public class OxygenPyloneController : MonoBehaviour
             lineRenderer = GetComponent<LineRenderer>();
         }
         isConnectedPlayer = false;
+        networkLineRenderer = networkConnection.GetComponent<LineRenderer>();
     }
 
     private void Update()
     {
+        //if the previous Pylone exists, we connect this pylone to it ==> SHOULD BE CALLED ONLY WHEN NETWORK CHANGES RATHER THAN EVERY FRAME
+        if (prevPylone != null)
+        {
+            prevPylone.GetComponent<OxygenPyloneController>().ConnectToPylone(gameObject);
+        }
+
+        //We now handle the player's detection :
         curDistance = (player.transform.position-transform.position).magnitude;
         Debug.Log("DISTANCE WITH PLAYER : " + curDistance);
         if (curDistance < maxDistance)
         {
             Debug.Log("CONNECTED TO OXYGEN NETWORK");
             isConnectedPlayer = true;
-            lineRenderer.positionCount = 2;
-            lineRenderer.SetPosition(0, transform.position);
-            lineRenderer.SetPosition(1, player.transform.position);
+            ConnectToPlayer();
             if (!oxygenController.isRecharging)
             {
                 StartOxygenFlow();
@@ -54,16 +70,42 @@ public class OxygenPyloneController : MonoBehaviour
         }
     }
 
+    /**
+     *  Function starting the oxygen flow towards the player
+     */
     private void StartOxygenFlow()
     {
         oxygenController.isRecharging = true;
         StartCoroutine(oxygenController.AddOxygen(oxygenController.consumptionRate));
     }
 
+    /**
+     *  Function stopping the oxygen flow
+     */
     private void StopOxygenFlow()
     {
         oxygenController.isRecharging = false;
         lineRenderer.positionCount = 0;
+    }
+
+    /**
+     *  Function that will visually connect to an inputted pylone
+     */
+    public void ConnectToPylone(GameObject connectPylone)
+    {
+        networkLineRenderer.positionCount = 2;
+        networkLineRenderer.SetPosition(0, transform.position);
+        networkLineRenderer.SetPosition(1, connectPylone.transform.position);
+    }
+
+    /**
+     *  Function that will visually connect to player
+     */
+    private void ConnectToPlayer()
+    {
+        lineRenderer.positionCount = 2;
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, player.transform.position);
     }
 
 }
