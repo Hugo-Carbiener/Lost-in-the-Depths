79cc6a371ca5633bbd7e8283ac7c2250e763b7b5
Eloïse Bonnet <eloise.bonnet@telecom-sudparis.eu>
2022-11-19 09:30:57 +0100

    Jump implemented

--- unijam2022/Assets/Scripts/PlayerMovement.cs
+++ unijam2022/Assets/Scripts/PlayerMovement.cs
@@ -4,10 +4,30 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody rb;
     [SerializeField] private float speed = 3.5f;
+    [SerializeField] private float jumpHeight = 3f;
+    [SerializeField] private float gravityScale = 5f;
+    private float jumpForce;
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        jumpForce = Mathf.Sqrt(jumpHeight * -2 * Physics.gravity.y);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(Physics.gravity * (gravityScale - 1) * rb.mass);
+    }
+
     private void Update()
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
         transform.Translate(movement * speed * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Z))
+        {
+            rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+        }
     }
 }
