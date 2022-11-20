10bfa1e78e1a323b92cc6e737ffb54a04b09abb2
Eloïse Bonnet <eloise.bonnet@telecom-sudparis.eu>
2022-11-20 14:07:33 +0100

    Music added

--- unijam2022/Assets/Scripts/SoundManager.cs
+++ unijam2022/Assets/Scripts/SoundManager.cs
@@ -6,6 +6,7 @@ public class SoundManager : MonoBehaviour
 {
     [SerializeField] private AudioSource natureSource;
     [SerializeField] private AudioSource depthSource;
+    [SerializeField] private AudioSource depthMusic;
     [SerializeField] private float depthTransition = -4f;
 
     private void Update()
@@ -14,11 +15,21 @@ public class SoundManager : MonoBehaviour
         {
             natureSource.volume -= Time.deltaTime * 0.1f;
             depthSource.volume += Time.deltaTime * 0.1f;
+            depthMusic.volume += Time.deltaTime * 0.1f;
         } 
         else if (PlayerManager._instance.gameObject.transform.position.y > depthTransition)
         {
             depthSource.volume -= Time.deltaTime * 0.1f;
+            depthMusic.volume -= Time.deltaTime * 0.1f;
             natureSource.volume += Time.deltaTime * 0.1f;
+        }
+        else if (PlayerManager._instance.gameObject.transform.position.y < -58f)
+        {
+            depthMusic.volume -= Time.deltaTime * 0.1f;
+        }
+        else if (PlayerManager._instance.gameObject.transform.position.y < depthTransition && PlayerManager._instance.gameObject.transform.position.y > -58f)
+        {
+            depthMusic.volume += Time.deltaTime * 0.1f;
         }
     }
 }
