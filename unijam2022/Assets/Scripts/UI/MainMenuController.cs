ad46a46ea05e10eae4a35d42cd88a8977f1d8abc
BiscuitPrime <henri.nomico@gmail.com>
2022-11-20 14:12:30 +0100

    Added loading screen

--- unijam2022/Assets/Scripts/UI/MainMenuController.cs
+++ unijam2022/Assets/Scripts/UI/MainMenuController.cs
@@ -1,14 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    [SerializeField] private Canvas loadingScreen;
+    [SerializeField] private TextMeshProUGUI text;
+
+    private void Start()
+    {
+        loadingScreen.gameObject.SetActive(false);
+    }
+
     public void OnPlay()
     {
+        StartCoroutine(StartGame());
+    }
+
+    private IEnumerator StartGame()
+    {
         Time.timeScale = 1;
-        SceneManager.LoadScene("MVP");
+        loadingScreen.gameObject.SetActive(true);
+        AsyncOperation loadingLevel = SceneManager.LoadSceneAsync("MVP");
+        while (!loadingLevel.isDone)
+        {
+            yield return null;
+        }
     }
 
     public void OnQuit()
