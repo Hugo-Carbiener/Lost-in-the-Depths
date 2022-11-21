c16ac14f6ac1b70c8205bdd4c3508bb7c34abec4
BiscuitPrime <henri.nomico@gmail.com>
2022-11-21 12:16:39 +0100

    Fixed Infinite Jump

--- unijam2022/Assets/Scripts/GroundDetector.cs
+++ unijam2022/Assets/Scripts/GroundDetector.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GroundDetector : MonoBehaviour
 {
+    [SerializeField] private GameObject feet;
+
     public bool isGrounded { get; private set; }
 
     private void Awake()
@@ -14,9 +16,13 @@ public class GroundDetector : MonoBehaviour
     private void OnCollisionStay(Collision collision)
     {
         Debug.Log("JUMP FIX : GDECTOR : STAY called");
+        RaycastHit hit;
         if (collision.gameObject.CompareTag("Grass") || collision.gameObject.CompareTag("Rock"))
         {
-            isGrounded = true;
+            if (Physics.Raycast(feet.transform.position, new Vector3(0, -1, 0), out hit, 1f) && (hit.transform.tag=="Grass" || hit.transform.tag == "Rock"))
+            {
+                isGrounded = true;
+            }
         }
     }
 
